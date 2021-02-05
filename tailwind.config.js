1ccd41edd0eade529a8c4be13876dd71b083b0b1
Skyclo <hello.skyclo@gmail.com>
2021-02-05 13:52:57 -0500

    💄 chore: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
-module.exports = {
-  purge: [],
+let tailwindConfig = {
+  purge: [
+    './static/**/*.js',
+    './*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Gilroy','ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe\\ UI"', 'Roboto', '"Helvetica\\ Neue"', 'Arial', '"Noto\\ Sans"', 'sans-serif', '"Apple\\ Color\\ Emoji"', '"Segoe\\ UI\\ Emoji"', '"Segoe\\ UI\\ Symbol"', '"Noto\\ Color\\ Emoji"'],
+      'serif': ['Georgia', 'Cambria', '"Times\\ New\\ Roman"', 'Times', 'serif'],
+      'mono': ['Cascadia Mono', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation\ Mono"', '"Courier\ New"', 'monospace']
+    },
     extend: {},
   },
   variants: {
@@ -9,3 +17,6 @@ module.exports = {
   },
   plugins: [],
 }
+
+
+module.exports = tailwindConfig
